3d6c86bf329e2015699e6751569d3bcd9e09290d
Lu Ken <bluewish.ken.lu@live.cn>
2024-12-22 04:08:14 +0800

    status: add some icons
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>

--- src/GenTradeAgent/src/renderer/src/components/StatusBar.vue
+++ src/GenTradeAgent/src/renderer/src/components/StatusBar.vue
@@ -1,24 +1,37 @@
-
 <template>
   <div class="status-main">
     <div class="status-notification">
       <span>Latest Prices</span>
     </div>
-    <div class="status-clock">Clock</div>
-    <div class="status-network">Network</div>
+    <div class="status-clock">
+      <span class="status-icon" >Version v0.1</span>
+    </div>
+    <div class="status-clock">
+      <n-icon>
+        <Clock20Regular class="status-icon"/>
+      </n-icon>
+    </div>
+    <div class="status-network">
+      <n-icon>
+        <PlugConnected20Filled class="status-icon"/>
+      </n-icon>
+    </div>
   </div>
 </template>
-
+<script setup lang="ts">
+import { PlugConnected20Filled, Clock20Regular } from '@vicons/fluent'
+</script>
 <style lang="scss" scoped>
 .status-main {
   display: flex;
   flex-direction: row;
   justify-content: center;
+  padding-right: 10px;
+  padding-left: 10px;
 }
 
 .status-notification {
   flex-grow: 1;
-  padding-left: 10px;
   align-content: center;
 }
 
@@ -28,8 +41,8 @@
   align-content: center;
 }
 
-.status-network {
-  padding-right: 10px;
+.status-icon {
+  width: 20px;
   height: 100%;
   align-content: center;
 }
