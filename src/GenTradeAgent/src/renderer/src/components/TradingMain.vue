9ca988f9c03c3b3fc7d93ce9d42d144062961dd6
Lu Ken <bluewish.ken.lu@live.cn>
2024-12-23 18:05:57 +0800

    Update doc (#9)
    * action: fix the missing file
    
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>
    
    * add title button and watermark
    
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>
    
    ---------
    
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>

--- src/GenTradeAgent/src/renderer/src/components/TradingMain.vue
+++ src/GenTradeAgent/src/renderer/src/components/TradingMain.vue
@@ -1,21 +1,43 @@
-<script setup lang="ts">
-import { Splitpanes, Pane } from 'splitpanes'
-import TradingDashboard  from './TradingDashboard.vue'
-import 'splitpanes/dist/splitpanes.css'
-import TradingChatAgent from './TradingChatAgent.vue';
-
-</script>
-
 <template>
   <splitpanes class="default-theme">
     <pane class="pane-dashboard">
-      <TradingDashboard />
+      <div class="pane-dashboard-title">
+        <n-space horizontal>
+          <n-select
+            v-model:value="selectedValue"
+            filterable
+            placeholder="Please select a song"
+            :options="options"
+            size="tiny"
+            menu-size="tiny"
+            class="title-item"
+          />
+        </n-space>
+      </div>
+      <TradingDashboard class="pane-dashboard-chart" />
     </pane>
     <pane class="pane-agent" min-size="20" max-size="30">
       <TradingChatAgent />
     </pane>
   </splitpanes>
 </template>
+
+<script setup lang="ts">
+import { Splitpanes, Pane } from 'splitpanes'
+import TradingDashboard from './TradingDashboard.vue'
+import 'splitpanes/dist/splitpanes.css'
+import TradingChatAgent from './TradingChatAgent.vue'
+import { NSpace, NSelect } from 'naive-ui'
+import { ref } from 'vue'
+
+const selectedValue = ref(null)
+const options = [
+  {
+    label: 'BTC',
+    value: 'BTC_USDT'
+  }
+]
+</script>
 
 <style lang="scss" scoped>
 .splitpanes {
@@ -26,8 +48,17 @@ import TradingChatAgent from './TradingChatAgent.vue';
 .pane-dashboard {
   background-color: #f3f3f3 !important;
   display: flex;
-  justify-content: center;
-  padding: 4px;
+  padding: 2px;
+  flex-direction: column;
+}
+
+.pane-dashboard-title {
+  height: 30px;
+  margin-bottom: 2px;
+}
+
+.pane-dashboard-chart {
+  flex-grow: 1;
 }
 
 .pane-agent {
@@ -35,5 +66,4 @@ import TradingChatAgent from './TradingChatAgent.vue';
   display: flex;
   padding: 4px;
 }
-
 </style>
